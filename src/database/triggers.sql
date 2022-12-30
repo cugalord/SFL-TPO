76b2645bfdbc90d9119235839996aa6864a629fe
Cugalord <nejc.ACDC@gmail.com>
2022-12-30 17:35:18 +0100

    procedure and triggers update

--- src/database/triggers.sql
+++ src/database/triggers.sql
@@ -1,34 +1,4 @@
 DELIMITER !!
-CREATE TRIGGER job_parcel_status_1
-AFTER UPDATE
-ON job FOR EACH ROW
-BEGIN
-    -- handover
-    IF new.job_type_id = 2 AND new.job_status_id=2 THEN
-        UPDATE parcel
-        SET parcel_status_id=2
-        WHERE id IN(
-            SELECT parcel_id
-            FROM job_packet
-            WHERE job_id=new.id
-        );
-    END IF;
-    -- delivered
-    IF new.job_type_id=8 AND new.job_status_id=2 THEN
-        UPDATE parcel
-        SET parcel_status_id=4
-        WHERE id IN(
-            SELECT parcel_id
-            FROM job_packet
-            WHERE job_id=new.id
-        );
-    END IF;
-END!!
-DELIMITER ;
--- ------------------------------------------
--- ------------------------------------------
--- ------------------------------------------
-DELIMITER !!
 CREATE TRIGGER job_parcel_status_2
 AFTER INSERT
 ON job FOR EACH ROW
@@ -52,21 +22,6 @@ BEGIN
             FROM job_packet
             WHERE job_id=new.id
         );
-    END IF;
-END!!
-DELIMITER ;
--- ------------------------------------------
--- ------------------------------------------
--- ------------------------------------------
-DELIMITER !!
-CREATE TRIGGER job_date_mark
-AFTER UPDATE
-ON job FOR EACH ROW
-BEGIN
-    IF new.job_status_id = 3 OR new.job_status_id=2 THEN
-        UPDATE job
-        SET date_completed=NOW()
-        WHERE id=old.id;
     END IF;
 END!!
 DELIMITER ;
